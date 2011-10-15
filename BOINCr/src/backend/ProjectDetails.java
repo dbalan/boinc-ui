ca99ce1d4f02aa4b4f81f6ec8a06cfef1ceb9abd
Dhananjay M Balan <mb.dhananjay@gmail.com>
2011-10-15 16:05:24 +0530

    Fixed Project Details, Everything working.

--- BOINCr/src/backend/ProjectDetails.java
+++ BOINCr/src/backend/ProjectDetails.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class ProjectDetails {
 	
-	String projectName;
+	
 	String niceProjectName; // User friendly project name;
 	int userNo;  // Total Number of volunteers in system.
 	int hostNo;  // Total no of hosts.
@@ -29,24 +29,25 @@ public class ProjectDetails {
 		workUnitNo = res.getInt(1);
 		
 		
-		String projCmd = "grep PROJECT "+projectPath+"/html/project/project.inc | head -n1 | cut -d , -f2 | cut -d \" -f2";
+		String projCmd = "grep PROJECT /var/tmp/boinc/docktest/html/project/project.inc";
 		proc = Main.Auth.execCommand(projCmd);
 		
 		reader=new BufferedReader(new InputStreamReader(proc.getInputStream()));
-		
-		
-		niceProjectName=reader.readLine(); 
+		String tempName = reader.readLine();
+		tempName = tempName.split(",")[1];
+		tempName = tempName.split("\\)")[0];
+		tempName = tempName.replaceAll("^\\s+", "");
+		niceProjectName=tempName; 
 		
 		 
 	}
 	public String projecthtml(){
 		String msg;
 		msg="<html><p align=\"center\"><table>";
-		msg+="<tr><td><b>Project Name</b></td><td>:</td><td>"+projectName+"</td><tr>";
-		msg+="<br><tr><td><b>Nice Project Name<b></td><td>:</td><td>"+niceProjectName+"</td><tr>";
-		msg+="<br><tr><td><b>User No</b></td><td>:</td><td>"+Integer.toString(userNo)+"</td><tr>";
-		msg+="<br><tr><td><b>Host No</b></td><td>:</td><td>"+Integer.toString(hostNo)+"</td><tr>";
-		msg+="<br><tr><td><b>Work Uint No</b></td><td>:</td><td>"+Integer.toString(workUnitNo)+"</td><tr>";
+		msg+="<tr><td><b>Project Name</b></td><td>:</td><td>"+niceProjectName+"</td><tr>";
+		msg+="<br><tr><td><b>Total Volunteers</b></td><td>:</td><td>"+Integer.toString(userNo)+"</td><tr>";
+		msg+="<br><tr><td><b>Total Hosts</b></td><td>:</td><td>"+Integer.toString(hostNo)+"</td><tr>";
+		msg+="<br><tr><td><b>Workunits Registred</b></td><td>:</td><td>"+Integer.toString(workUnitNo)+"</td><tr>";
 		msg+="</table><p></html>";
 		return msg;
 		
